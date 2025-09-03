af77978071be8388085358a4cdc41d4ebe904247
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2025-09-03 11:41:37 +0900

    Playerの影状態のアニメーション追加してみた

--- GraduationWork/GraduationWork/Scene/GamaScene/InGame/InGameScene.cpp
+++ GraduationWork/GraduationWork/Scene/GamaScene/InGame/InGameScene.cpp
@@ -190,7 +190,7 @@ void InGameScene::SetStage()
 				object_manager.CreateObject<Block>(world_pos, block_size);
 				break;
 			case PLAYER:
-				object_manager.CreateObject<Player>(world_pos, Vector2D(48.0f, 64.0f));
+				object_manager.CreateObject<Player>(world_pos, Vector2D(45.0f, 64.0f));
 				break;
 			case WALL:
 				object_manager.CreateObject<Wall>(world_pos, Vector2D(48.0f, 48.0f));
